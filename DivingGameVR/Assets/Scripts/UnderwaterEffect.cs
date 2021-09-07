6db4d7c5fbbdf0dc9b3ab9cb21f41739ba074ff0
Viktor Rüdén <viktor.ruden2@gmail.com>
2021-09-07 13:58:33 +0200

    made a fog effect on the camera and made so you can change the underwatereffect in the inspector

--- DivingGameVR/Assets/Scripts/UnderwaterEffect.cs
+++ DivingGameVR/Assets/Scripts/UnderwaterEffect.cs
@@ -6,10 +6,22 @@ using UnityEngine;
 public class UnderwaterEffect : MonoBehaviour
 {
     public Material _mat;
+    [Range(0.001f, 0.1f)]
+    public float _pixelOffset;
+    [Range(0.1f, 20f)]
+    public float _noiseScale;
+    [Range(0.1f, 20f)]
+    public float _noiseFrequency;
+    [Range(0.1f, 30f)]
+    public float _noiseSpeed;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _mat.SetFloat("_NoiseFrequency", _noiseFrequency);
+        _mat.SetFloat("_NoiseSpeed", _noiseSpeed);
+        _mat.SetFloat("_NoiseScale", _noiseScale);
+        _mat.SetFloat("_PixelOffset", _pixelOffset);
     }
 
     // Update is called once per frame

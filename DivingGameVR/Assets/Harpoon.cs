27a9c1bfe5cfefa44b88ed57911f5737c4379fbc
SeasterSwe <Jakobbulow01@gmail.com>
2021-09-20 13:58:06 +0200

    bövijgkuh

--- DivingGameVR/Assets/Harpoon.cs
+++ DivingGameVR/Assets/Harpoon.cs
@@ -1,26 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Harpoon : MonoBehaviour
 {
-    private GameObject spearObj;
     private Spear spear;
     public float spearVelocity;
     public bool canShoot;
-    // Start is called before the first frame update
+    Vector3 spearPos;
+    Quaternion spearRot;
+    //private MouseClick input;
 
     private void Awake()
     {
         spear = GetComponentInChildren<Spear>();
-        spearObj = spear.gameObject;
         canShoot = true;
-    }
-    public void Shoot()
-    {
-        if(canShoot)
-        {
+        spearPos = spear.transform.position;
+        spearRot = spear.transform.rotation;
+        //input = new MouseClick();
 
+        //input.Player.Mouse.performed += Shoot;
+        //input.Player.Mouse.Enable();
+    }
+
+    private void Update()
+    {
+        if (ControllerManager.Instance.rightTrigger > 0.2f)
+            Shoot();
+    }
+
+    public void Shoot(/*InputAction.CallbackContext context*/)
+    {
+        if (canShoot)
+        {
+            spear.ActivateSpear();
+            spear.Shoot(spearVelocity);
+            canShoot = false;
         }
+        else
+        {
+            RetriverSpear();
+        }
+    }
+    public void RetriverSpear()
+    {
+        spear.DeactivateSpear();
+        spear.transform.position = spearPos;
+        transform.rotation = spearRot;
+
+        canShoot = true;
     }
 }

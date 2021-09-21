1921a925fd2ef4b0f634efbe89cef4f12dc79dfd
RobinVondrak <robinvondrak01@gmail.com>
2021-09-21 14:09:27 +0200

    Revert "Merge branch 'main' into Znork"
    This reverts commit 9399563e9871d4a6f4f6d905b244a4bc85dbba45, reversing
    changes made to 7f9afc28aab499a0ad36074f695684b7a10bc321.

--- DivingGameVR/Assets/Harpoon.cs
+++ DivingGameVR/Assets/Harpoon.cs
@@ -1,54 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class Harpoon : MonoBehaviour
 {
+    private GameObject spearObj;
     private Spear spear;
     public float spearVelocity;
     public bool canShoot;
-    Vector3 spearPos;
-    Quaternion spearRot;
-    //private MouseClick input;
+    // Start is called before the first frame update
 
     private void Awake()
     {
         spear = GetComponentInChildren<Spear>();
+        spearObj = spear.gameObject;
         canShoot = true;
-        spearPos = spear.transform.position;
-        spearRot = spear.transform.rotation;
-        //input = new MouseClick();
-
-        //input.Player.Mouse.performed += Shoot;
-        //input.Player.Mouse.Enable();
     }
-
-    private void Update()
+    public void Shoot()
     {
-        if (ControllerManager.Instance.rightTrigger > 0.2f)
-            Shoot();
-    }
-
-    public void Shoot(/*InputAction.CallbackContext context*/)
-    {
-        if (canShoot)
+        if(canShoot)
         {
-            spear.ActivateSpear();
-            spear.Shoot(spearVelocity);
-            canShoot = false;
-        }
-        else
-        {
-            RetriverSpear();
-        }
-    }
-    public void RetriverSpear()
-    {
-        spear.DeactivateSpear();
-        spear.transform.position = spearPos;
-        transform.rotation = spearRot;
 
-        canShoot = true;
+        }
     }
 }

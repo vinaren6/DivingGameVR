63206b2b8b473fa4fa72683efd2fe76253d9ec35
SeasterSwe <Jakobbulow01@gmail.com>
2021-09-29 14:39:14 +0200

    Stuff and i need coffe

---
+++ DivingGameVR/Assets/ScoreManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ScoreManager : MonoBehaviour
+{
+    private static ScoreManager _instance;
+
+    public static ScoreManager Instance { get { return _instance; } }
+
+    private int score = 0;
+    TextMeshPro[] texts;
+    private void Awake()
+    {
+        texts = GetComponentsInChildren<TextMeshPro>();
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+        score = 0;
+    }
+
+    public void AddScore()
+    {
+        score += 1;
+    }
+
+    public void UpdateText()
+    {
+        foreach(TextMeshPro text in texts)
+        {
+            text.text = "Catches: " + score.ToString();
+        }
+    }
+}

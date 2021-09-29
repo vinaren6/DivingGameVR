5f83eed5aa65146f5d2dbdbfbba1117948a6b316
Viktor Rüdén <viktor.ruden2@gmail.com>
2021-09-29 11:23:30 +0200

    added corals to ocean floor

---
+++ DivingGameVR/Assets/Scripts/RotateModels.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateModels : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+     
+        transform.Rotate(-90, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
